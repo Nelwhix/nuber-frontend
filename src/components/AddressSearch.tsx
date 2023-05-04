01a2db6b7c3992599e49d3e495cf112b9dec8d26
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-05-04 01:02:01 +0100

    used google place autocomplete

--- src/components/AddressSearch.tsx
+++ src/components/AddressSearch.tsx
@@ -1,23 +1,61 @@
-import { AddressAutofill } from "@mapbox/search-js-react";
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+  } from 'react-places-autocomplete';
 
 export default function AddressSearch({ address, setAddress }: AddressSearchProps) {
-    const accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY as string
 
-    const handleRetrieve = (res: any) => {
-        console.log('fired')
-        console.log(res)
+    const fetchCoordinates = async (address: string) => {
+        try {
+            const results = await geocodeByAddress(address)
+            const latLng = await getLatLng(results[0])
+            console.log(latLng)
+        } catch (err) {
+            console.log(err)
+        }
+        
     }
-
-    return <AddressAutofill accessToken={accessToken} onRetrieve={handleRetrieve}>
-        <input
-            type="text"
-            placeholder="My destination"
-            className="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm"
-            autoComplete="street-address"
+    
+    return  <PlacesAutocomplete
             value={address}
-            onChange={(e) => setAddress(e.target.value)}
-        />
-    </AddressAutofill>
+            onChange={setAddress}
+            onSelect={fetchCoordinates}
+        >
+            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'My destination',
+                className: 'location-search-input mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+        
+    </PlacesAutocomplete>
+   
 }
 
 interface AddressSearchProps {
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Main, Head, NextScript } from 'next/document'
 
 export default function Document() {
+
+    const maps_key = process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY
   return (
     <Html lang="en" data-theme="light">
         <Head>
             <title>Nuber</title>
+            <script src={`https://maps.googleapis.com/maps/api/js?key=${maps_key}&libraries=places`}></script>
         </Head>
       <body className='min-h-screen bg-gray-100 text-center antialiased'>
         <Main />
